4fdfe2e2eba02f34f7d076d4f1d2ce630400c811
Mario Segundo Galindo <mario@m2mario.local>
2024-10-13 18:06:10 +0200

    finalize the app and fix transactions error

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -6,7 +6,7 @@ import Image from 'next/image'
 import Link from 'next/link'
 import { usePathname } from 'next/navigation'
 import Footer from './Footer'
-//import PlaidLink from './PlaidLink'
+import PlaidLink from './PlaidLink'
 
 const Sidebar = ({ user }: SiderbarProps) => {
   const pathname = usePathname();
@@ -19,10 +19,10 @@ const Sidebar = ({ user }: SiderbarProps) => {
             src="/icons/logo.svg"
             width={34}
             height={34}
-            alt="Horizon logo"
+            alt="Nexus logo"
             className="size-[24px] max-xl:size-14"
           />
-          <h1 className="sidebar-logo">Horizon</h1>
+          <h1 className="sidebar-logo">Nexus</h1>
         </Link>
 
         {sidebarLinks.map((item) => {
@@ -49,7 +49,7 @@ const Sidebar = ({ user }: SiderbarProps) => {
           )
         })}
         
-        {/*<PlaidLink user={user} />*/}
+        <PlaidLink user={user} />
       </nav>
 
       <Footer user={user} />
